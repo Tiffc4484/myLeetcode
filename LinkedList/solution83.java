0ca2fa722cc9f6c95f0e7b97aac4f5638da18d59
Tiffc4484 <tiffanyc4484@gmail.com>
2021-03-25 00:53:08 -0700

    organize files into folders

---
+++ LinkedList/solution83.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class solution83 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {this.val = val;}
+        ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+    }
+    public ListNode deleteDuplicates(ListNode head){
+        if (head == null) {
+            return head;
+        }
+        ListNode dummy = head;
+        while (dummy.next != null) {
+            if (dummy.val != dummy.next.val) {
+                dummy = dummy.next;
+            } else {
+                if (dummy.next.next != null) {
+                    dummy.next = dummy.next.next;
+                } else {
+                    dummy.next = null;
+                }
+            }
+        }
+        return head;
+    }
+}
