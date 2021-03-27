0c946029ce5788d2e30201c0e420a390363456bb
Tiffc4484 <tiffanyc4484@gmail.com>
2021-03-26 23:32:09 -0700

    206 reverse a linked list

---
+++ LinkedList/solution203.java
@@ -0,0 +1,26 @@
+public class solution203 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {this.val = val;}
+        ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+
+        ListNode prev = sentinel;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return sentinel.next;
+    }
+}
