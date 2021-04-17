75c99ad303a9d7e031b9fda5ce55cd800bc3bd1e
Tiffc4484 <tiffanyc4484@gmail.com>
2021-04-17 02:44:14 -0700

    13 roman to int

---
+++ String/src/solution13.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class solution13 {
+    public static int romanToInt(String s) {
+        HashMap<Character,Integer> roman = new HashMap<>();
+        roman.put('I',1);
+        roman.put('V',5);
+        roman.put('X',10);
+        roman.put('L',50);
+        roman.put('C',100);
+        roman.put('D',500);
+        roman.put('M',1000);
+        int toReturn = 0;
+        int last = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            toReturn += roman.get(current);
+            if (roman.get(current) > last) {
+                toReturn = toReturn -  2 * last;
+            }
+            last = roman.get(current);
+        }
+        return toReturn;
+    }
+}
